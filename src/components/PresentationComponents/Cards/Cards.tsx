7b9191893f9582f45bac4bd135efb26aa4bd9df8
RoshawnL <roshawnlayne@gmail.com>
2026-03-17 15:04:21 -0400

    added title to icon for document references

--- src/components/PresentationComponents/Cards/Cards.tsx
+++ src/components/PresentationComponents/Cards/Cards.tsx
@@ -298,6 +298,7 @@ const VoyageCard = () => {
                                   key={`${index}-${uuidv4()}`}
                                   className="fa fa-file-text"
                                   aria-hidden="true"
+                                  title="View Source Document"
                                 ></i>,
                               );
                               additionalStyles.borderColor = 'blue';
